610b2ac3e4db6c76518b43ddd96ad95b84f0cd63
TU NGUYEN <trongtunguyen08@icloud.com>
2024-06-15 17:08:32 +0700

    Integrate SwiftData

---
+++ SwiftDataResearch/Core/AddExpense/Views/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  SwiftDataResearch
+//
+//  Created by Tu Nguyen on 15/6/24.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
---
+++ SwiftDataResearch/Models/Expense.swift
@@ -0,0 +1,8 @@
+//
+//  Expense.swift
+//  SwiftDataResearch
+//
+//  Created by Tu Nguyen on 15/6/24.
+//
+
+import Foundation
