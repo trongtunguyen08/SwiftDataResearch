ebe888fdc8a761aac166b1533587fd57b96da0de
TU NGUYEN <trongtunguyen08@icloud.com>
2024-06-15 15:38:52 +0700

    Initial Commit

---
+++ SwiftDataResearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataResearch
+//
+//  Created by Tu Nguyen on 15/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataResearch/SwiftDataResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataResearchApp.swift
+//  SwiftDataResearch
+//
+//  Created by Tu Nguyen on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
